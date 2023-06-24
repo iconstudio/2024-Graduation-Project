6750b3aab0c2ff8feaa2893c1b830e15043b067c
Iconer <yoyofa2@hotmail.com>
2023-06-24 09:56:22 +0900

    Hello World!

---
+++ 2024-Graduation-Project/Main.cpp
@@ -0,0 +1,10 @@
+import <iostream>;
+import <string>;
+
+int main([[maybe_unused]] const int& argc, [[maybe_unused]] const char* const* const& argv)
+{
+	std::cout << "Hello World!\n";
+
+	return 0;
+}
+
