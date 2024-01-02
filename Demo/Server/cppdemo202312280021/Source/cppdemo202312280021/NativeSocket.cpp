e5b5480f7ebc4c3d48cbc05f0ddc42b961b36895
Iconer <yoyofa2@hotmail.com>
2024-01-03 08:43:56 +0900

    `FNativeSocket`'s factory methods are body-only defined

--- Demo/Server/cppdemo202312280021/Source/cppdemo202312280021/NativeSocket.cpp
+++ Demo/Server/cppdemo202312280021/Source/cppdemo202312280021/NativeSocket.cpp
@@ -12,3 +12,58 @@ const noexcept
 {
 	return NULL != GetHandle();
 }
+
+FNativeSocket
+FNativeSocket::Create(EIoSynchronousType type, const EInternetProtocol& protocol, const EIpAddressFamily& family)
+noexcept
+{
+	return FNativeSocket();
+}
+
+FNativeSocket
+FNativeSocket::Create(EIoSynchronousType type, const EInternetProtocol& protocol, const EIpAddressFamily& family, EErrorCode& error_code)
+noexcept
+{
+	return FNativeSocket();
+}
+
+bool
+FNativeSocket::TryCreate(EIoSynchronousType type, const EInternetProtocol& protocol, const EIpAddressFamily& family, FAttentSocket& out)
+noexcept
+{
+	return false;
+}
+
+bool
+FNativeSocket::TryCreate(EIoSynchronousType type, const EInternetProtocol& protocol, const EIpAddressFamily& family, FAttentSocket& out, EErrorCode& error_code)
+noexcept
+{
+	return false;
+}
+
+Expected<FNativeSocket, EErrorCode>
+FNativeSocket::TryCreate(EIoSynchronousType type, const EInternetProtocol& protocol, const EIpAddressFamily& family)
+noexcept
+{
+	return Expected<FNativeSocket, EErrorCode>();
+}
+
+void
+FNativeSocket::InternalSetAddressReusable(FNativeSocket& target, bool& flag)
+noexcept
+{
+	target.SetAddressReusable(flag);
+}
+
+void
+FNativeSocket::SetAddressReusable(const bool flag)
+noexcept
+{
+}
+
+bool
+FNativeSocket::GetAddressReusable()
+const noexcept
+{
+	return false;
+}
