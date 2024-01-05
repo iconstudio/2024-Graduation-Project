2014836228ad67323163f1fd11a5c31f601feacb
Iconer <yoyofa2@hotmail.com>
2024-01-06 05:39:15 +0900

    Network error getters

--- Demo/Server/cppdemo202312280021/Source/cppdemo202312280021/Private/IconerBlueprinter.cpp
+++ Demo/Server/cppdemo202312280021/Source/cppdemo202312280021/Private/IconerBlueprinter.cpp
@@ -1,1 +1,17 @@
+#pragma comment(lib, "Ws2_32.lib")
 #include "IconerBlueprinter.h"
+#include <WinSock2.h>
+
+uint32
+UNetworkUtility::AcquireNetworkErrorByInteger()
+noexcept
+{
+	return ::GetLastError();
+}
+
+EErrorCode
+UNetworkUtility::AcquireNetworkError()
+noexcept
+{
+	return static_cast<EErrorCode>(::GetLastError());
+}
