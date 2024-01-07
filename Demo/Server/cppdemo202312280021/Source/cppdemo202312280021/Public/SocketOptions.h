cf26e8e3c12788f315b3b2de8fa2d1aa9e08b01f
Iconer <yoyofa2@hotmail.com>
2024-01-07 16:05:49 +0900

    `ESocketOptions` is moved to the new header

---
+++ Demo/Server/cppdemo202312280021/Source/cppdemo202312280021/Public/SocketOptions.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "CoreMinimal.h"
+
+UENUM(BlueprintType, Blueprintable)
+enum class [[nodiscard]] ESocketOptions
+{
+	SO_UNKNOWN = 0x0000 UMETA(DisplayName = "Unknown"),
+	SO_DEBUG = 0x0001 UMETA(DisplayName = "Debug"),
+	SO_REUSEADDR = 0x0004 UMETA(DisplayName = "Recyclable"),
+	SO_DONTROUTE = 0x0010 UMETA(DisplayName = "Don't Route"),
+	SO_BROADCAST = 0x0020 UMETA(DisplayName = "Broadcast"),
+	SO_USELOOPBACK = 0x0040 UMETA(DisplayName = "Use Loopback"),
+	SO_LINGER = 0x0080 UMETA(DisplayName = "Linger"),
+	TCP_NODELAY = 0x0001 UMETA(DisplayName = "No Delay"),
+	SO_DONTLINGER = static_cast<int32>(~0x0080) UMETA(DisplayName = "Don't Linger"),
+	SO_KEEPALIVE = 0x0008 UMETA(DisplayName = "Keep Alive"),
+	SO_UPDATE_ACCEPT_CONTEXT = 0x700B UMETA(DisplayName = "Update"), // MSWSock.h
+};
