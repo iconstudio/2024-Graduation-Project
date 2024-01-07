069a21bcabc0961d8372ba7023721a1b368c4bb1
Iconer <yoyofa2@hotmail.com>
2024-01-08 03:09:17 +0900

    `FSocketTask` is moved to the new header

---
+++ Demo/Server/cppdemo202312280021/Source/cppdemo202312280021/Private/SocketTask.cpp
@@ -0,0 +1,21 @@
+#include "SocketTask.h"
+#include <thread>
+
+USocketJob::USocketJob()
+	: FRunnable()
+{
+}
+
+USocketJob::~USocketJob()
+{
+}
+
+void FSocketTask::Start() const
+{
+
+}
+
+void FSocketTask::StartAsync() const
+{
+
+}
