8b89bd7d07f1051bf6667e01fca506058a7eb529
Iconer <yoyofa2@hotmail.com>
2024-01-09 21:41:06 +0900

    `USocket`'s methods are defined
    Signed-off-by: Iconer <yoyofa2@hotmail.com>

--- Demo/Server/cppdemo202312280021/Source/cppdemo202312280021/Socket.h
+++ Demo/Server/cppdemo202312280021/Source/cppdemo202312280021/Socket.h
@@ -12,16 +12,43 @@ class CPPDEMO202312280021_API USocket : public UActorComponent
 public:	
 	USocket();
 
-	//UFUNCTION()
-	//bool Connect() const noexcept;
+	UFUNCTION()
+	bool Connect(const FEndpoint& endpoint) const noexcept;
+	UFUNCTION()
+	bool ConnectTo(FStringView ip_address, uint16 port) const noexcept;
+	UFUNCTION()
+	bool Listen() const noexcept;
+	UFUNCTION()
+	FNativeSocket Accept() const noexcept;
+	UFUNCTION()
+	FNativeSocket ReserveAccept(const FNativeSocket&) const noexcept;
+	UFUNCTION()
+	FNativeSocket ReserveAccept(USocket& socket_component) const noexcept;
+	UFUNCTION()
+	FNativeSocket ReserveAccept(USocket* const socket_component) const noexcept;
+	UFUNCTION()
+	bool Disconnect() const noexcept
+
+	UFUNCTION()
+	FNativeSocket& GetNativeHandle() noexcept
+	UFUNCTION()
+	const FNativeSocket& GetNativeHandle() const noexcept
 
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 protected:
+	UFUNCTION(BlueprintSetter)
+	void SetReuseAddress(bool) noexcept;
+	UFUNCTION(BlueprintGetter)
+	bool GetReuseAddress() const noexcept;
+	UFUNCTION()
+
 	virtual void BeginPlay() override;
 
 	UPROPERTY(EditInstanceOnly, meta = (DisplayName = "Socket"))
 	FNativeSocket myHandle;
+	UPROPERTY(EditInstanceOnly, BlueprintSetter = SetReuseAddress, BlueprintGetter = GetReuseAddress)
+	bool DoesReuseAddress;
 };
 
 namespace net::ue
