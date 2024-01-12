b0b5f775e656265687e314627958301319010daa
Iconer <yoyofa2@hotmail.com>
2024-01-12 20:01:22 +0900

    `UFEndpointFactory`

--- Demo/Server/cppdemo202312280021/Source/cppdemo202312280021/Private/EndPoint.cpp
+++ Demo/Server/cppdemo202312280021/Source/cppdemo202312280021/Private/EndPoint.cpp
@@ -1,6 +1,7 @@
 #pragma comment(lib, "Ws2_32.lib")
 #include "EndPoint.h"
 #include <WinSock2.h>
+#include <utility>
 
 FEndpoint::FEndpoint(const FIpAddress& ip, const uint16& port)
 noexcept
@@ -11,3 +12,17 @@ FEndpoint::FEndpoint(FIpAddress&& ip, const uint16& port)
 noexcept
 	: myAddress(static_cast<FIpAddress&&>(ip)), myPort(::htons(port))
 {}
+
+FEndpoint
+UFEndpointFactory::CreateEndPoint(FString address, EIpAddressFamily family, int32 port)
+noexcept
+{
+	return FEndpoint{ FIpAddress{ family, std::move(address) }, static_cast<uint16>(port) };
+}
+
+FEndpoint
+UFEndpointFactory::CreateEndPointBy(const FIpAddress& ip_address, int32 port)
+noexcept
+{
+	return FEndpoint{ ip_address, static_cast<uint16>(port) };
+}
