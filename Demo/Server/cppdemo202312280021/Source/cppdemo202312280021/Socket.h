b0cbfc33b8cd170e634595251752c1ad0b5b5d2a
Iconer <yoyofa2@hotmail.com>
2024-01-12 20:57:31 +0900

    Specifiers of `UFUNCTION` are modified
    Signed-off-by: Iconer <yoyofa2@hotmail.com>

--- Demo/Server/cppdemo202312280021/Source/cppdemo202312280021/Socket.h
+++ Demo/Server/cppdemo202312280021/Source/cppdemo202312280021/Socket.h
@@ -13,24 +13,24 @@ public:
 	USocket();
 	virtual ~USocket() noexcept override;
 
-	UFUNCTION()
-	void Attach(FNativeSocket& native_socket) noexcept;
-	UFUNCTION()
-	bool Connect(const FEndpoint& endpoint) const noexcept;
-	UFUNCTION()
-	UFUNCTION()
+	UFUNCTION(BlueprintCallable, CallInEditor)
+	void Attach(UPARAM(ref) FNativeSocket& native_socket) noexcept;
+	UFUNCTION(BlueprintCallable, CallInEditor)
+	bool Connect(UPARAM(ref) const FEndpoint& endpoint) const noexcept;
+	UFUNCTION(BlueprintCallable, CallInEditor)
 	bool ConnectTo(FString ip_address, int32 port) const noexcept;
+	UFUNCTION(BlueprintCallable, CallInEditor)
 	bool Listen() const noexcept;
 	UFUNCTION()
 	FNativeSocket Accept() const noexcept;
 	UFUNCTION()
-	bool ReserveAcceptByNativeSocket(FIoContext& context, FNativeSocket& native_socket) const noexcept;
+	bool ReserveAcceptByNativeSocket(UPARAM(ref) FIoContext& context, UPARAM(ref) FNativeSocket& native_socket) const noexcept;
 	UFUNCTION()
-	bool ReserveAccept(FIoContext& context, USocket* socket_component) const noexcept;
-	UFUNCTION()
+	bool ReserveAccept(UPARAM(ref) FIoContext& context, USocket* socket_component) const noexcept;
+	UFUNCTION(BlueprintCallable, CallInEditor)
 	bool Disconnect() const noexcept;
-	UFUNCTION()
-	bool DisconnectAsync(FIoContext& context) const noexcept;
+	UFUNCTION(BlueprintCallable, CallInEditor)
+	bool DisconnectAsync(UPARAM(ref) FIoContext& context) const noexcept;
 
 	UFUNCTION()
 	const FNativeSocket& GetNativeHandle() const noexcept;
@@ -40,9 +40,9 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
-	UFUNCTION(BlueprintSetter)
+	UFUNCTION(BlueprintCallable, BlueprintSetter, CallInEditor)
 	void SetReuseAddress(bool flag) noexcept;
-	UFUNCTION(BlueprintGetter)
+	UFUNCTION(BlueprintPure, BlueprintGetter, CallInEditor)
 	bool GetReuseAddress() const noexcept;
 
 	UPROPERTY(EditInstanceOnly, meta = (DisplayName = "Socket"))
