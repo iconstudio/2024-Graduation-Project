1c8e9a4bff2d75a980023af800bdb7bb02f40b6a
강승호 <112061429+kang-seung-ho@users.noreply.github.com>
2024-01-14 01:07:52 +0900

    NetworkManager, PlayerCharacterFactory 추가

---
+++ Demo/SagaGameWorld/Source/SagaGameWorld/PlayerCharacterFactory.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacterFactory.h"
+
+ASagaGameWorldCharacter* UPlayerCharacterFactory::CreateCharacter(int32 PlayerID, FVector Location)
+{
+	return nullptr;
+}
