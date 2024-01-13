f428aae713f2e3399e67547019cfccce98a24a89
Iconer <yoyofa2@hotmail.com>
2024-01-14 01:45:28 +0900

    `FBasicPacket`

---
+++ Demo/Server/cppdemo202312280021/Source/cppdemo202312280021/Public/BasicPacket.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "PacketType.h"
+#include "BasicPacket.generated.h"
+
+USTRUCT(Atomic, BlueprintType, Blueprintable, meta = (DisplayName = "Basic Packet"))
+struct CPPDEMO202312280021_API FBasicPacket
+{
+	constexpr FBasicPacket() noexcept = default;
+	constexpr ~FBasicPacket() noexcept = default;
+
+	[[nodiscard]]
+	virtual size_t GetByteSize() const noexcept
+	{
+		return sizeof(myType);
+	}
+	
+	[[nodiscard]]
+	virtual TSharedPtr<uint8> Serialize() const
+	{
+		return nullptr;
+	}
+
+	[[nodiscard]]
+	virtual TSharedPtr<uint8> Serialize(TSharedPtr<uint8> buffer) const
+	{
+		return buffer;
+	}
+
+protected:
+	UPROPERTY(NoClear, VisibleAnywhere, BlueprintEditable, meta = (DisplayName = "Internet Protocol"))
+	EPacketType myType;
+};
