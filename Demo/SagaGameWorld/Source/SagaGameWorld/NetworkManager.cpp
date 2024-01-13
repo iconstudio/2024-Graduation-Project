6a312f4a29a490278a5ac1ba7bc459f41c3c718a
강승호 <112061429+kang-seung-ho@users.noreply.github.com>
2024-01-14 01:07:52 +0900

    NetworkManager, PlayerCharacterFactory 추가

---
+++ Demo/SagaGameWorld/Source/SagaGameWorld/NetworkManager.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NetworkManager.h"
+
+// Sets default values
+ANetworkManager::ANetworkManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ANetworkManager::AssignPlayerID(APlayerController* PlayerController)
+{
+
+}
+
+void ANetworkManager::HandlePlayerDisconnected(int32 PlayerID)
+{
+
+}
+
+// Called when the game starts or when spawned
+void ANetworkManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+//void ANetworkManager::Tick(float DeltaTime)
+//{
+//	Super::Tick(DeltaTime);
+//
+//}
+
