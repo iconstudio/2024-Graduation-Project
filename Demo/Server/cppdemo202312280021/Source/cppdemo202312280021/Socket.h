3c4abd94c6e10469fadf02ea62301e629f012966
Iconer <yoyofa2@hotmail.com>
2024-01-14 18:14:19 +0900

    `USocket`'s meta data are modified

--- Demo/Server/cppdemo202312280021/Source/cppdemo202312280021/Socket.h
+++ Demo/Server/cppdemo202312280021/Source/cppdemo202312280021/Socket.h
@@ -13,26 +13,26 @@ public:
 	USocket();
 	virtual ~USocket() noexcept override;
 
-	UFUNCTION(BlueprintCallable, CallInEditor)
+	UFUNCTION(BlueprintCallable, CallInEditor, Category = "Iconer")
 	void Attach(UPARAM(ref) FNativeSocket& native_socket) noexcept;
-	UFUNCTION(BlueprintCallable, CallInEditor)
+	UFUNCTION(BlueprintCallable, CallInEditor, Category = "Iconer")
 	bool Connect(UPARAM(ref) const FEndpoint& endpoint) const noexcept;
-	UFUNCTION(BlueprintCallable, CallInEditor)
+	UFUNCTION(BlueprintCallable, CallInEditor, Category = "Iconer")
 	bool ConnectTo(FString ip_address, int32 port) const noexcept;
-	UFUNCTION(BlueprintCallable, CallInEditor)
+	UFUNCTION(BlueprintCallable, CallInEditor, Category = "Iconer")
 	bool Listen() const noexcept;
-	UFUNCTION()
+	UFUNCTION(Category = "Iconer")
 	FNativeSocket Accept() const noexcept;
-	UFUNCTION()
+	UFUNCTION(Category = "Iconer")
 	bool ReserveAcceptByNativeSocket(UPARAM(ref) FIoContext& context, UPARAM(ref) FNativeSocket& native_socket) const noexcept;
-	UFUNCTION()
+	UFUNCTION(Category = "Iconer")
 	bool ReserveAccept(UPARAM(ref) FIoContext& context, USocket* socket_component) const noexcept;
-	UFUNCTION(BlueprintCallable, CallInEditor)
+	UFUNCTION(BlueprintCallable, CallInEditor, Category = "Iconer")
 	bool Disconnect() const noexcept;
-	UFUNCTION(BlueprintCallable, CallInEditor)
+	UFUNCTION(BlueprintCallable, CallInEditor, Category = "Iconer")
 	bool DisconnectAsync(UPARAM(ref) FIoContext& context) const noexcept;
 
-	UFUNCTION()
+	UFUNCTION(Category = "Iconer")
 	const FNativeSocket& GetNativeHandle() const noexcept;
 
 	virtual void TickComponent(float dt, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
@@ -40,14 +40,14 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
-	UFUNCTION(BlueprintCallable, BlueprintSetter, CallInEditor)
+	UFUNCTION(BlueprintCallable, BlueprintSetter, CallInEditor, Category = "Iconer")
 	void SetReuseAddress(bool flag) noexcept;
-	UFUNCTION(BlueprintPure, BlueprintGetter, CallInEditor)
+	UFUNCTION(BlueprintPure, BlueprintGetter, CallInEditor, Category = "Iconer")
 	bool GetReuseAddress() const noexcept;
 
-	UPROPERTY(EditInstanceOnly, meta = (DisplayName = "Socket"))
+	UPROPERTY(EditInstanceOnly, Category = "Iconer", meta = (DisplayName = "Socket"))
 	FNativeSocket myHandle;
-	UPROPERTY(EditInstanceOnly, BlueprintSetter = SetReuseAddress, BlueprintGetter = GetReuseAddress)
+	UPROPERTY(EditInstanceOnly, BlueprintSetter = SetReuseAddress, BlueprintGetter = GetReuseAddress, Category = "Iconer")
 	bool DoesReuseAddress;
 };
 
