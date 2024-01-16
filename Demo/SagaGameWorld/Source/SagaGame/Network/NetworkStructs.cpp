5d95b3eebe4b1999fa25036a4e5d62dc969fedb9
강승호 <112061429+kang-seung-ho@users.noreply.github.com>
2024-01-16 10:31:02 +0900

    구조체 올바르게 생성 및 컴파일 버그 수정, 플레이어 컨트롤러 생성

---
+++ Demo/SagaGameWorld/Source/SagaGame/Network/NetworkStructs.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Network/NetworkStructs.h"
+
+NetworkStructs::NetworkStructs()
+{
+}
+
+NetworkStructs::~NetworkStructs()
+{
+}
