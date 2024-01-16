59fdb51729a5cebd2a03b9db00aa62feb243a4fd
Iconer <yoyofa2@hotmail.com>
2024-01-16 15:10:40 +0900

    The `ReadOnlyProperty` would not be used at `iconer::User` and just be used with a delegate
    Signed-off-by: Iconer <yoyofa2@hotmail.com>

--- Demo/Server/Server/Iconer/Network/Private/User.cpp
+++ Demo/Server/Server/Iconer/Network/Private/User.cpp
@@ -1,9 +1,7 @@
 module Iconer.Network.User;
 import <iostream>;
 
-iconer::User::User() noexcept
-	: myStatus(this, iconer::user_status::None{}, User::_StateDelegate)
-{}
+//: myStatus(this, iconer::user_status::None{}, User::StateDelegate)
 
 void
 iconer::User::OnNetworkInitialized(bool succeed, net::ErrorCodes error_code) noexcept
@@ -14,20 +12,30 @@ iconer::User::OnNetworkInitialized(bool succeed, net::ErrorCodes error_code) noe
 }
 
 void
-iconer::User::_StateDelegate(User& user, state_data_t& state)
+iconer::User::StateDelegate(User& user, state_data_t& state)
 {
 	state.Visit(
 		[&]<typename T>(T&& value) noexcept {
 			using U = std::decay_t<T>;
 			if constexpr (std::same_as<U, user_status::None>)
-			{}
+			{
+				std::cout << "Player Status: None\n";
+			}
 			else if constexpr (std::same_as<U, user_status::Listening>)
-			{}
+			{
+				std::cout << "Player Status: Listening\n";
+			}
 			else if constexpr (std::same_as<U, user_status::Connecting>)
-			{}
+			{
+				std::cout << "Player Status: Connecting\n";
+			}
 			else if constexpr (std::same_as<U, user_status::Idle>)
-			{}
+			{
+				std::cout << "Player Status: Idle\n";
+			}
 			else if constexpr (std::same_as<U, user_status::Closing>)
-			{}
+			{
+				std::cout << "Player Status: Closing\n";
+			}
 		});
 }
