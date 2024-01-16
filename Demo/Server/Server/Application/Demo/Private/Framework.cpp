5abea82ce2647cee15dc69f251b032dbb95c277b
Iconer <yoyofa2@hotmail.com>
2024-01-16 21:41:33 +0900

    Working on server application

--- Demo/Server/Server/Application/Demo/Private/Framework.cpp
+++ Demo/Server/Server/Application/Demo/Private/Framework.cpp
@@ -4,6 +4,7 @@ module;
 #include <MSWSock.h>
 
 module Demo.Framework;
+import <memory>;
 // ReSharper disable CppMemberFunctionMayBeStatic
 
 demo::Framework::Framework(size_t clients_count, std::uint16_t port)
@@ -16,23 +17,40 @@ demo::Framework::Framework(size_t clients_count, std::uint16_t port)
 void
 demo::Framework::Awake()
 {
-	
+	iconer::user_id_t id = startUserID;
+
+	for (size_t i = 0; i < everyUsers.GetCapacity(); ++i)
+	{
+		auto ptr = std::make_unique<iconer::User>(id++);
+		if (not ptr)
+		{
+			throw "Cannot allocate an user entity";
+		}
+
+		ptr->SetState<iconer::user_status::None>();
+
+		everyUsers.Add(std::move(ptr));
+	}
 }
 
 bool
 demo::Framework::Start() noexcept
 {
+	for (auto& user_ptr : everyUsers)
+	{
+		auto user = user_ptr.get();
+
+		if (user->SetState<iconer::user_status::Listening>())
+		{}
+	}
+
 	return false;
 }
 
 void
 demo::Framework::Update()
-{
-	
-}
+{}
 
 void
 demo::Framework::Cleanup() noexcept
-{
-	
-}
+{}
--- Demo/Server/Server/Server.cpp
+++ Demo/Server/Server/Server.cpp
@@ -7,16 +7,19 @@ import Iconer.Network.UserManager;
 import Demo.Framework;
 import <iostream>;
 
-int main()
+demo::Framework appFramework{ 50ULL, 40000U };
+
+int
+main()
 {
 	std::cout << "Hello World!\n";
 
 	//iconer::NetworkEntityManager<iconer::user_id_t> entity_manager{ 20ULL };
 	iconer::UserManager user_manager{ 20ULL };
 
-	iconer::User u1;
+	iconer::User u1{ 1 };
 	u1.OnNetworkInitialized(true, {});
-	iconer::User u2;
+	iconer::User u2{ 2 };
 	u2.OnNetworkInitialized(false, {});
 
 	auto change_0 = u1.SetState<iconer::user_status::Connecting>();
@@ -26,7 +29,7 @@ int main()
 	auto change_4 = u1.SetState<iconer::user_status::Listening>();
 
 	//entity_manager.Add(std::move(u1));
-	user_manager.Add(std::move(u2));
+	//user_manager.Add(std::move(u2));
 
 	//iconer::NetworkView* v = &u1;
 	//v->OnNetworkInitialized(false, {});
