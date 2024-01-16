1c4109474f80189da98bc2922fd5265090ae0264
Iconer <yoyofa2@hotmail.com>
2024-01-16 14:55:49 +0900

    Working on `Demo.Framework`
    Signed-off-by: Iconer <yoyofa2@hotmail.com>

---
+++ Demo/Server/Server/Application/Demo/Private/Framework.cpp
@@ -0,0 +1,34 @@
+module;
+#pragma comment(lib, "Ws2_32.lib")
+#include <WinSock2.h>
+#include <MSWSock.h>
+module Demo.Framework;
+
+demo::Framework::Framework()
+{
+	
+}
+
+void
+demo::Framework::Awake()
+{
+	
+}
+
+bool
+demo::Framework::Start() noexcept
+{
+	return false;
+}
+
+void
+demo::Framework::Update()
+{
+	
+}
+
+void
+demo::Framework::Cleanup() noexcept
+{
+	
+}
