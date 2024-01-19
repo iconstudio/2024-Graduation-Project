2c27d5a435d38538d59e6d1d043172783c47583f
Iconer <yoyofa2@hotmail.com>
2024-01-19 16:50:01 +0900

    The `main` would not print messages
    Signed-off-by: Iconer <yoyofa2@hotmail.com>

--- Demo/Server/Server/Server.cpp
+++ Demo/Server/Server/Server.cpp
@@ -6,15 +6,12 @@ import Iconer.Network.EntityManager;
 import Iconer.Network.UserManager;
 import Demo.Framework;
 import Iconer.Container.FlatMap;
-import <iostream>;
 
 demo::Framework appFramework{ 50ULL, 40000U };
 
 int
 main()
 {
-	std::cout << "Server is started!\n";
-
 	appFramework.Awake();
 	appFramework.Start();
 	appFramework.Update();
