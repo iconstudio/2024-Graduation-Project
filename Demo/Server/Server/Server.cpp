fdcce09381c6e00f35fe020fbb25ef1cb29126f5
Iconer <yoyofa2@hotmail.com>
2024-01-20 05:05:30 +0900

    Cleanup the `main` source

--- Demo/Server/Server/Server.cpp
+++ Demo/Server/Server/Server.cpp
@@ -1,11 +1,6 @@
 ﻿#pragma comment(lib, "Modern Network.lib")
 
-import Iconer.Declarations;
-import Iconer.Network.User;
-import Iconer.Network.EntityManager;
-import Iconer.Network.UserManager;
 import Demo.Framework;
-import Iconer.Container.FlatMap;
 
 demo::Framework appFramework{ 50ULL, 40000U };
 
