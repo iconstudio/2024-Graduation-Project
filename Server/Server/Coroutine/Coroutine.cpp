453b29656129bd6d6ebc41d022dbef2807bf1223
Iconer <yoyofa2@hotmail.com>
2024-01-21 18:28:26 +0900

    `Coroutine`

---
+++ Server/Server/Coroutine/Coroutine.cpp
@@ -0,0 +1,48 @@
+module Iconer.Coroutine;
+import <thread>;
+
+void
+iconer::coroutine::Coroutine::Start()
+{
+	if (myHandle and not isTriggered)
+	{
+		isTriggered = true;
+
+		myHandle();
+	}
+}
+
+void
+iconer::coroutine::Coroutine::StartAsync()
+{
+	if (myHandle and not isTriggered)
+	{
+		isTriggered = true;
+		static const auto pred = [](std::coroutine_handle<void> handle) -> void {
+			if (handle)
+			{
+				handle();
+			}
+		};
+
+		std::thread{ pred, myHandle }.detach();
+	}
+}
+
+void
+iconer::coroutine::Coroutine::Resume()
+{
+	if (myHandle and isTriggered)
+	{
+		myHandle();
+	}
+}
+
+void
+iconer::coroutine::Coroutine::operator()()
+{
+	if (myHandle and isTriggered)
+	{
+		myHandle();
+	}
+}
