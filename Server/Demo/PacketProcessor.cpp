b88b20711597ed4fb6ceef8e23a077968294db3e
Iconer <yoyofa2@hotmail.com>
2024-01-29 13:59:43 +0900

    Packet enum events are predefined

--- Server/Demo/PacketProcessor.cpp
+++ Server/Demo/PacketProcessor.cpp
@@ -11,7 +11,7 @@ demo::PacketProcessor(demo::Framework& framework
 	, std::uintptr_t session_raw_id
 	, void* packet_handle, size_t handle_size)
 {
-	auto packet_ptr = std::launder(reinterpret_cast<app::BasicPacket*>(packet_handle));
+	auto packet_ptr = std::launder(static_cast<app::BasicPacket*>(packet_handle));
 	if (nullptr == packet_ptr)
 	{
 		throw "Null packet error.";
@@ -138,5 +138,70 @@ demo::PacketProcessor(demo::Framework& framework
 		{
 		}
 		break;
+
+		case app::PacketProtocol::SC_SIGNIN_SUCCESS:
+			break;
+		case app::PacketProtocol::SC_SIGNIN_FAILURE:
+			break;
+		case app::PacketProtocol::SC_SIGNUP_SUCCESS:
+			break;
+		case app::PacketProtocol::SC_SIGNUP_FAILURE:
+			break;
+		case app::PacketProtocol::SC_SERVER_INFO:
+			break;
+		case app::PacketProtocol::SC_RESPOND_ROOMS:
+			break;
+		case app::PacketProtocol::SC_RESPOND_USERS:
+			break;
+		case app::PacketProtocol::SC_RESPOND_VERSION:
+			break;
+		case app::PacketProtocol::SC_ROOM_CREATED_BY_SELF:
+			break;
+		case app::PacketProtocol::SC_ROOM_CREATED:
+			break;
+		case app::PacketProtocol::SC_ROOM_CREATE_FAILED:
+			break;
+		case app::PacketProtocol::SC_ROOM_DESTROYED:
+			break;
+		case app::PacketProtocol::SC_ROOM_JOINED:
+			break;
+		case app::PacketProtocol::SC_ROOM_JOIN_FAILED:
+			break;
+		case app::PacketProtocol::SC_ROOM_LEFT:
+			break;
+		case app::PacketProtocol::SC_ROOM_MEMBER_LEFT:
+			break;
+		case app::PacketProtocol::SC_GAME_START:
+			break;
+		case app::PacketProtocol::SC_GAME_VICTORY:
+			break;
+		case app::PacketProtocol::SC_GAME_DEFEAT:
+			break;
+		case app::PacketProtocol::SC_CREATE_PLAYER:
+			break;
+		case app::PacketProtocol::SC_CREATE_OBJET:
+			break;
+		case app::PacketProtocol::SC_CREATE_VFX:
+			break;
+		case app::PacketProtocol::SC_MOVE_CHARACTER:
+			break;
+		case app::PacketProtocol::SC_MOVE_OBJET:
+			break;
+		case app::PacketProtocol::SC_UPDATE_CHARACTER:
+			break;
+		case app::PacketProtocol::SC_UPDATE_OBJET:
+			break;
+		case app::PacketProtocol::SC_REMOVE_CHARACTER:
+			break;
+		case app::PacketProtocol::SC_REMOVE_OBJET:
+			break;
+		case app::PacketProtocol::SC_PLAY_SFX:
+			break;
+		case app::PacketProtocol::SC_PLAY_MUSIC:
+			break;
+		case app::PacketProtocol::SC_ANIMATION_START:
+			break;
+		case app::PacketProtocol::SC_CHAT:
+			break;
 	}
 }
