d22bb0a68cd623f3c1d2587053903c86012b5bfb
Iconer <yoyofa2@hotmail.com>
2024-01-31 22:57:21 +0900

    `ASagaPlayerController` is modified

--- Demo/SagaGameWorld/Source/SagaGame/Player/SagaPlayerController.cpp
+++ Demo/SagaGameWorld/Source/SagaGame/Player/SagaPlayerController.cpp
@@ -22,7 +22,7 @@ ASagaPlayerController::SetupInputComponent()
     //JumpKey = USagaPlayerInputSettings::GetJumpKey();
 
     // Jump 액션에 대한 입력 바인딩
-    //InputComponent->BindAction("Jump", IE_Pressed, this, &ASagaPlayerController::Jump);
+	//InputComponent->BindAction("Jump", IE_Pressed, this, &ASagaPlayerController::Jump);
 }
 
 void
