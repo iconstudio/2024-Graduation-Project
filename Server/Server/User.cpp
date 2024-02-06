f7537e42bb048b26ee819e09ba30c0ee5f65c7ee
Iconer <yoyofa2@hotmail.com>
2024-02-07 06:07:05 +0900

    `User::Awake()` is not inline

--- Server/Server/User.cpp
+++ Server/Server/User.cpp
@@ -2,6 +2,14 @@ module;
 
 module Iconer.Application.User;
 
+void
+iconer::app::User::Awake()
+{
+	ContextType::Clear();
+	recvOffset = 0;
+	myName.reserve(nicknameLength);
+}
+
 iconer::app::User::IoResult
 iconer::app::User::SendSignInPacket()
 {
