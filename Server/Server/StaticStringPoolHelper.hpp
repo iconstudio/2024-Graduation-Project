18cee06b63a373452693b5510e5bf63b4f9005cd
Iconer <yoyofa2@hotmail.com>
2024-02-12 02:25:50 +0900

    Bug is fixed

--- Server/Server/StaticStringPoolHelper.hpp
+++ Server/Server/StaticStringPoolHelper.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #define StoreStaticString(index, string) template<>\
-struct iconer::util::BasicStaticStringPool<std::remove_cvref_t<decltype(string[0])>, 0>\
+struct iconer::util::BasicStaticStringPool<std::remove_cvref_t<decltype(string[0])>, index>\
 {\
 	static inline constexpr auto value = std::basic_string_view{ string };\
 }
