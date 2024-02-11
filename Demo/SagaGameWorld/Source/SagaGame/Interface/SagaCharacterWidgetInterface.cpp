dd493665b0884516db725e9dbbad637480337677
강승호 <112061429+kang-seung-ho@users.noreply.github.com>
2024-02-12 01:36:29 +0900

    캐릭터 스탯이 피격시 줄어들도록 HP와 연동, 사망시 HP바도 지워주도록 구현

---
+++ Demo/SagaGameWorld/Source/SagaGame/Interface/SagaCharacterWidgetInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interface/SagaCharacterWidgetInterface.h"
+
+// Add default functionality here for any ISagaCharacterWidgetInterface functions that are not pure virtual.
