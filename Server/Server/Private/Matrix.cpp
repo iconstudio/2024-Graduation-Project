a21e823196ce7ab0fbb4b1cae276d5cb489a8b39
Iconer <yoyofa2@hotmail.com>
2024-02-13 14:23:04 +0900

    `Matrix` is refactored

--- Server/Server/Private/Matrix.cpp
+++ Server/Server/Private/Matrix.cpp
@@ -68,6 +68,42 @@ noexcept
 	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(this), concurrent_lhs * concurrent_rhs);
 }
 
+void
+iconer::util::d3d::Matrix::LocalMultiplyImplementation(const float& rhs)
+noexcept
+{
+	auto concurrent_lhs = DirectX::XMLoadFloat4x4(reinterpret_cast<const DirectX::XMFLOAT4X4*>(this));
+
+	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(this), concurrent_lhs * rhs);
+}
+
+void
+iconer::util::d3d::Matrix::LocalMultiplyImplementation(const double& rhs)
+noexcept
+{
+	auto concurrent_lhs = DirectX::XMLoadFloat4x4(reinterpret_cast<const DirectX::XMFLOAT4X4*>(this));
+
+	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(this), concurrent_lhs * static_cast<float>(rhs));
+}
+
+void
+iconer::util::d3d::Matrix::LocalMultiplyImplementation(float&& rhs)
+noexcept
+{
+	auto concurrent_lhs = DirectX::XMLoadFloat4x4(reinterpret_cast<const DirectX::XMFLOAT4X4*>(this));
+
+	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(this), concurrent_lhs * std::move(rhs));
+}
+
+void
+iconer::util::d3d::Matrix::LocalMultiplyImplementation(double&& rhs)
+noexcept
+{
+	auto concurrent_lhs = DirectX::XMLoadFloat4x4(reinterpret_cast<const DirectX::XMFLOAT4X4*>(this));
+
+	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(this), concurrent_lhs * static_cast<float>(rhs));
+}
+
 iconer::util::d3d::Matrix
 iconer::util::d3d::Matrix::MultiplyImplementation(const Matrix& lhs, const Matrix& rhs)
 noexcept
@@ -107,6 +143,54 @@ noexcept
 	return result;
 }
 
+iconer::util::d3d::Matrix
+iconer::util::d3d::Matrix::MultiplyImplementation(const Matrix& lhs, const float& value)
+noexcept
+{
+	auto concurrent_lhs = DirectX::XMLoadFloat4x4(reinterpret_cast<const DirectX::XMFLOAT4X4*>(std::addressof(lhs)));
+
+	Matrix result{};
+	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(std::addressof(result)), concurrent_lhs * value);
+
+	return result;
+}
+
+iconer::util::d3d::Matrix
+iconer::util::d3d::Matrix::MultiplyImplementation(const Matrix& lhs, float&& value)
+noexcept
+{
+	auto concurrent_lhs = DirectX::XMLoadFloat4x4(reinterpret_cast<const DirectX::XMFLOAT4X4*>(std::addressof(lhs)));
+
+	Matrix result{};
+	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(std::addressof(result)), concurrent_lhs * std::move(value));
+
+	return result;
+}
+
+iconer::util::d3d::Matrix
+iconer::util::d3d::Matrix::MultiplyImplementation(const Matrix& lhs, const double& value)
+noexcept
+{
+	auto concurrent_lhs = DirectX::XMLoadFloat4x4(reinterpret_cast<const DirectX::XMFLOAT4X4*>(std::addressof(lhs)));
+
+	Matrix result{};
+	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(std::addressof(result)), concurrent_lhs * static_cast<float>(value));
+
+	return result;
+}
+
+iconer::util::d3d::Matrix
+iconer::util::d3d::Matrix::MultiplyImplementation(const Matrix& lhs, double&& value)
+noexcept
+{
+	auto concurrent_lhs = DirectX::XMLoadFloat4x4(reinterpret_cast<const DirectX::XMFLOAT4X4*>(std::addressof(lhs)));
+
+	Matrix result{};
+	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(std::addressof(result)), concurrent_lhs * static_cast<float>(value));
+
+	return result;
+}
+
 void
 iconer::util::d3d::Matrix::LocalDivideImplementation(const float& value)
 noexcept
@@ -122,7 +206,7 @@ noexcept
 {
 	auto concurrent_lhs = DirectX::XMLoadFloat4x4(reinterpret_cast<const DirectX::XMFLOAT4X4*>(this));
 
-	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(this), concurrent_lhs / value);
+	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(this), concurrent_lhs / static_cast<float>(value));
 }
 
 void
@@ -140,7 +224,7 @@ noexcept
 {
 	auto concurrent_lhs = DirectX::XMLoadFloat4x4(reinterpret_cast<const DirectX::XMFLOAT4X4*>(this));
 
-	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(this), concurrent_lhs / std::move(value));
+	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(this), concurrent_lhs / static_cast<float>(value));
 }
 
 iconer::util::d3d::Matrix
@@ -162,7 +246,7 @@ noexcept
 	auto concurrent_lhs = DirectX::XMLoadFloat4x4(reinterpret_cast<const DirectX::XMFLOAT4X4*>(std::addressof(lhs)));
 
 	Matrix result{};
-	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(std::addressof(result)), concurrent_lhs / value);
+	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(std::addressof(result)), concurrent_lhs / static_cast<float>(value));
 
 	return result;
 }
@@ -186,7 +270,7 @@ noexcept
 	auto concurrent_lhs = DirectX::XMLoadFloat4x4(reinterpret_cast<const DirectX::XMFLOAT4X4*>(std::addressof(lhs)));
 
 	Matrix result{};
-	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(std::addressof(result)), concurrent_lhs / std::move(value));
+	DirectX::XMStoreFloat4x4(reinterpret_cast<DirectX::XMFLOAT4X4*>(std::addressof(result)), concurrent_lhs / static_cast<float>(value));
 
 	return result;
 }
