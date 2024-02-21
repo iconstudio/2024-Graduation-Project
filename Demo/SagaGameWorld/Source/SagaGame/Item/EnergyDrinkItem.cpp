946b5da10df7736dd274f30a25bcb3feefafbaaa
강승호 <112061429+kang-seung-ho@users.noreply.github.com>
2024-02-21 11:44:50 +0900

    소모성 아이템 사용시 소멸되도록 구현

--- Demo/SagaGameWorld/Source/SagaGame/Item/EnergyDrinkItem.cpp
+++ Demo/SagaGameWorld/Source/SagaGame/Item/EnergyDrinkItem.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Item/EnergyDrinkItem.h"
+#include "InventoryComponent.h"
 #include "Character/SagaCharacterPlayer.h"
 
 void UEnergyDrinkItem::Use(ASagaCharacterPlayer* Character)
@@ -9,5 +10,10 @@ void UEnergyDrinkItem::Use(ASagaCharacterPlayer* Character)
 	if (Character)
 	{
 		Character->PlayerHP += HealAmount;
+
+		if (OwningInventory)
+		{
+			OwningInventory->RemoveItem(this);
+		}
 	}
 }
