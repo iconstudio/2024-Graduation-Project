6208de49b81e33cef92c51d861aede2d3dadacb5
Iconer <yoyofa2@hotmail.com>
2024-02-24 21:24:18 +0900

    `ReceiveRoomLeftPacket()`

--- Server/Test Client/Test Client Receiver.cpp
+++ Server/Test Client/Test Client Receiver.cpp
@@ -19,7 +19,8 @@ test::ReceiveSignInSucceedPacket(const std::byte* buffer)
 	return offset;
 }
 
-const std::byte* test::ReceiveSignInFailurePacket(const std::byte* buffer, int& error)
+const std::byte*
+test::ReceiveSignInFailurePacket(const std::byte* buffer, int& error)
 {
 	iconer::app::packets::SC_FailedSignInPacket pk{};
 	auto offset = pk.Read(recv_space);
@@ -29,7 +30,8 @@ const std::byte* test::ReceiveSignInFailurePacket(const std::byte* buffer, int& 
 	return offset;
 }
 
-const std::byte* test::ReceiveRoomCreatedPacket(const std::byte* buffer, IdType& room_id)
+const std::byte*
+test::ReceiveRoomCreatedPacket(const std::byte* buffer, IdType& room_id)
 {
 	iconer::app::packets::SC_RoomCreatedPacket pk{};
 	auto offset = pk.Read(recv_space);
@@ -39,7 +41,8 @@ const std::byte* test::ReceiveRoomCreatedPacket(const std::byte* buffer, IdType&
 	return offset;
 }
 
-const std::byte* test::ReceiveRoomCreationFailedPacket(const std::byte* buffer, iconer::app::RoomContract& error)
+const std::byte*
+test::ReceiveRoomCreationFailedPacket(const std::byte* buffer, iconer::app::RoomContract& error)
 {
 	iconer::app::packets::SC_RoomCreationFailedPacket pk{};
 	auto offset = pk.Read(recv_space);
@@ -49,8 +52,7 @@ const std::byte* test::ReceiveRoomCreationFailedPacket(const std::byte* buffer, 
 	return offset;
 }
 
-const
-std::byte* test::ReceiveRoomJoinedPacket(const std::byte* buffer, IdType& client_id)
+const std::byte* test::ReceiveRoomJoinedPacket(const std::byte* buffer, IdType& client_id)
 {
 	iconer::app::packets::SC_RoomJoinedPacket pk{};
 	auto offset = pk.Read(recv_space);
@@ -60,13 +62,24 @@ std::byte* test::ReceiveRoomJoinedPacket(const std::byte* buffer, IdType& client
 	return offset;
 }
 
-const
-std::byte* test::ReceiveRoomJoinFailedPacket(const std::byte* buffer, iconer::app::RoomContract& error)
+const std::byte*
+test::ReceiveRoomJoinFailedPacket(const std::byte* buffer, iconer::app::RoomContract& error)
 {
 	iconer::app::packets::SC_RoomJoinFailedPacket pk{};
 	auto offset = pk.Read(recv_space);
 
 	error = pk.errCause;
+
+	return offset;
+}
+
+const std::byte*
+test::ReceiveRoomLeftPacket(const std::byte* buffer, IdType& client_id)
+{
+	iconer::app::packets::SC_RoomLeftPacket pk{};
+	auto offset = pk.Read(recv_space);
+
+	client_id = pk.clientId;
 
 	return offset;
 }
