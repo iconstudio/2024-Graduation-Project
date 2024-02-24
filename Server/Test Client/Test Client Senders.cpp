2dd4a81e51649e929be6227f30cbe81272876875
Iconer <yoyofa2@hotmail.com>
2024-02-25 00:39:16 +0900

    Testing

--- Server/Test Client/Test Client Senders.cpp
+++ Server/Test Client/Test Client Senders.cpp
@@ -2,48 +2,47 @@ module TestClient;
 import Iconer.Application.BasicPacket;
 import Iconer.Application.Packet;
 
-iconer::net::Socket::AsyncResult
+iconer::net::Socket::IoResult
 test::SendSignInPacket()
 {
 	const iconer::app::packets::CS_SignInPacket sign_packet{ my_name.data(), my_name.size() };
 
 	auto ptr = sign_packet.Serialize();
 
-	iconer::net::IoContext send_ctx{};
-	return app_socket.Send(send_ctx, ptr.get(), sign_packet.WannabeSize());
+	return app_socket.Send(ptr.get(), sign_packet.WannabeSize());
 }
 
-iconer::net::Socket::AsyncResult
+iconer::net::Socket::IoResult
 test::SendCreateRoomPacket(std::wstring_view title)
 {
 	const iconer::app::packets::CS_CreateRoomPacket pk{ title.data(), title.length() };
 
 	auto ptr = pk.Serialize();
 
-	iconer::net::IoContext send_ctx{};
-	return app_socket.Send(send_ctx, ptr.get(), pk.WannabeSize());
+	auto r = app_socket.Send(ptr.get(), pk.WannabeSize());
+	delete[] ptr.release();
+
+	return std::move(r);
 }
 
-iconer::net::Socket::AsyncResult
+iconer::net::Socket::IoResult
 test::SendJoinRoomPacket(IdType room_id)
 {
 	const iconer::app::packets::CS_EnterRoomPacket pk{ room_id };
 
 	auto ptr = pk.Serialize();
 
-	iconer::net::IoContext send_ctx{};
-	return app_socket.Send(send_ctx, ptr.get(), pk.WannabeSize());
+	return app_socket.Send(ptr.get(), pk.WannabeSize());
 }
 
-iconer::net::Socket::AsyncResult
+iconer::net::Socket::IoResult
 test::SendLeaveRoomPacket()
 {
 	iconer::app::packets::CS_LeaveRoomPacket pk{};
 
 	auto ptr = pk.Serialize();
 
-	iconer::net::IoContext send_ctx{};
-	return app_socket.Send(send_ctx, ptr.get(), pk.WannabeSize());
+	return app_socket.Send(ptr.get(), pk.WannabeSize());
 }
 
 bool
@@ -53,14 +52,11 @@ test::SendPositionPacket()
 
 	auto serialized = position_pk.Serialize();
 
-	iconer::net::IoContext send_position_ctx{};
-	auto sent_position_r = app_socket.Send(send_position_ctx, serialized.get(), iconer::app::packets::CS_UpdatePositionPacket::WannabeSize());
+	auto sent_position_r = app_socket.Send(serialized.get(), iconer::app::packets::CS_UpdatePositionPacket::WannabeSize());
 	if (not sent_position_r.has_value())
 	{
 		return false;
 	}
-
-	send_position_ctx.Clear();
 
 	return true;
 }
