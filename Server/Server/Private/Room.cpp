828cccc1a457beed7114bdd6fcf06f64a73467c5
Iconer <yoyofa2@hotmail.com>
2024-02-25 15:55:37 +0900

    `Room` would serialize `SC_RespondMembersPacket` often

--- Server/Server/Private/Room.cpp
+++ Server/Server/Private/Room.cpp
@@ -1,3 +1,7 @@
+module;
+#include <memory>
+#include <mutex>
+
 module Iconer.Application.Room;
 import Iconer.Application.Packet;
 
@@ -6,4 +10,27 @@ iconer::app::Room::Awake()
 noexcept
 {
 	preRespondMembersPacket = std::make_unique<std::byte[]>(packets::SC_RespondMembersPacket::MaxSize());
+}
+
+std::span<std::byte>
+iconer::app::Room::SerializeMembers()
+{
+	std::unique_lock lock{ myLock };
+
+	if (isMemberUpdated)
+	{
+		packets::SC_RespondMembersPacket pk{};
+
+		for (auto& member : myMembers)
+		{
+			if (nullptr != member)
+			{
+				pk.AddMember(member->GetID(), member->GetName());
+			}
+		}
+
+		pk.Write(preRespondMembersPacket.get());
+	}
+
+	return std::span<std::byte>{ preRespondMembersPacket.get(), pk.WannabeSize() };
 }
