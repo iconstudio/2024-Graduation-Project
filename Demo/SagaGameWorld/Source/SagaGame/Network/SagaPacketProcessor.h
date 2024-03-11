9d2f2232153b411f1796255a6019c2771065a66f
Iconer <yoyofa2@hotmail.com>
2024-03-11 09:05:19 +0900

    송신 함수 헤더 분리

--- Demo/SagaGameWorld/Source/SagaGame/Network/SagaPacketProcessor.h
+++ Demo/SagaGameWorld/Source/SagaGame/Network/SagaPacketProcessor.h
@@ -2,22 +2,10 @@
 #include "CoreTypes.h"
 #include "Containers/UnrealString.h"
 
-#include <optional>
 #include "SagaRoomContract.h"
 
 namespace saga
 {
-	std::optional<int32> SendSignInPacket();
-	std::optional<int32> SendCreateRoomPacket(FString title);
-	std::optional<int32> SendJoinRoomPacket(int32 room_id);
-	std::optional<int32> SendLeaveRoomPacket();
-	std::optional<int32> SendRequestVersionPacket();
-	std::optional<int32> SendRequestRoomsPacket();
-	std::optional<int32> SendRequestMembersPacket();
-	std::optional<int32> SendGameStartPacket();
-	std::optional<int32> SendGameIsLoadedPacket();
-	std::optional<int32> SendPositionPacket();
-
 	const std::byte* ReceiveSignInSucceedPacket(const std::byte* buffer, int32& my_id);
 	const std::byte* ReceiveSignInFailurePacket(const std::byte* buffer, int32& error);
 	const std::byte* ReceiveRoomCreatedPacket(const std::byte* buffer, int32& room_id);
