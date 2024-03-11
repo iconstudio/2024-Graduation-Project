da6968ac588739c74d06ef279cbeb3e332d6627d
Iconer <yoyofa2@hotmail.com>
2024-03-11 09:05:19 +0900

    송신 함수 헤더 분리

---
+++ Demo/SagaGameWorld/Source/SagaGame/Network/SagaPacketSenders.cpp
@@ -0,0 +1,84 @@
+#include "SagaPacketSenders.h"
+#include "CoreMinimal.h"
+
+#include "SagaNetwork.h"
+
+std::optional<int32>
+saga::SendSignInPacket()
+{
+	auto& socket = USagaNetwork::GetLocalSocket();
+
+	return std::nullopt;
+}
+
+std::optional<int32>
+saga::SendCreateRoomPacket(FString title)
+{
+	auto& socket = USagaNetwork::GetLocalSocket();
+
+	return std::nullopt;
+}
+
+std::optional<int32>
+saga::SendJoinRoomPacket(int32 room_id)
+{
+	auto& socket = USagaNetwork::GetLocalSocket();
+
+	return std::nullopt;
+}
+
+std::optional<int32>
+saga::SendLeaveRoomPacket()
+{
+	auto& socket = USagaNetwork::GetLocalSocket();
+
+	return std::nullopt;
+}
+
+std::optional<int32>
+saga::SendRequestVersionPacket()
+{
+	auto& socket = USagaNetwork::GetLocalSocket();
+
+	return std::nullopt;
+}
+
+std::optional<int32>
+saga::SendRequestRoomsPacket()
+{
+	auto& socket = USagaNetwork::GetLocalSocket();
+
+	return std::nullopt;
+}
+
+std::optional<int32>
+saga::SendRequestMembersPacket()
+{
+	auto& socket = USagaNetwork::GetLocalSocket();
+
+	return std::nullopt;
+}
+
+std::optional<int32>
+saga::SendGameStartPacket()
+{
+	auto& socket = USagaNetwork::GetLocalSocket();
+
+	return std::nullopt;
+}
+
+std::optional<int32>
+saga::SendGameIsLoadedPacket()
+{
+	auto& socket = USagaNetwork::GetLocalSocket();
+
+	return std::nullopt;
+}
+
+std::optional<int32>
+saga::SendPositionPacket()
+{
+	auto& socket = USagaNetwork::GetLocalSocket();
+
+	return std::nullopt;
+}
