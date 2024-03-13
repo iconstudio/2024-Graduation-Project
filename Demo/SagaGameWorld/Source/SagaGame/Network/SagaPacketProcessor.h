f57162065628b2c9fbdbb1a376d1e50bb2c069eb
Iconstudio <yoyofa2@hotmail.com>
2024-03-13 14:56:16 +0900

    User and rooms events
    (cherry picked from commit 630ed39425c2d5463381beff5d12aa43897e2642)

--- Demo/SagaGameWorld/Source/SagaGame/Network/SagaPacketProcessor.h
+++ Demo/SagaGameWorld/Source/SagaGame/Network/SagaPacketProcessor.h
@@ -3,6 +3,7 @@
 #include "Containers/UnrealString.h"
 
 #include "SagaRoomContract.h"
+#include "SagaServerPacketPrefabs.h"
 
 namespace saga
 {
@@ -13,5 +14,8 @@ namespace saga
 	const std::byte* ReceiveRoomJoinedPacket(const std::byte* buffer, int32& client_id, int32& room_id);
 	const std::byte* ReceiveRoomJoinFailedPacket(const std::byte* buffer, RoomContract& error);
 	const std::byte* ReceiveRoomLeftPacket(const std::byte* buffer, int32& client_id);
+	const std::byte* ReceiveRespondVersionPacket(const std::byte* buffer, wchar_t* version_str_buffer, const size_t& str_length);
+	const std::byte* ReceiveRespondRoomsPacket(const std::byte* buffer, std::vector<datagrams::SerializedRoom>& rooms);
+	const std::byte* ReceiveRespondUsersPacket(const std::byte* buffer, std::vector<datagrams::SerializedMember>& users);
 
 }
