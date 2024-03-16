973cc95e75ca19f3ec09d2a6b5f1effd9e81411b
iconer <yoyofa2@hotmail.com>
2024-03-17 03:40:16 +0900

    `ISagaNetworkView`

---
+++ Demo/SagaGameWorld/Source/SagaNetwork/Private/Saga/Network/SagaNetworkView.cpp
@@ -0,0 +1,16 @@
+#include "Saga/Network/SagaNetworkView.h"
+
+void
+ISagaNetworkView::OnConnected_Implementation()
+{
+}
+
+void
+ISagaNetworkView::OnFailedToConnect_Implementation(int32 reason)
+{
+}
+
+void
+ISagaNetworkView::OnDisconnected_Implementation()
+{
+}
