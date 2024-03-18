c418dcef407f7eb102cc7845a5e2a247d3ada466
iconer <yoyofa2@hotmail.com>
2024-03-18 18:16:01 +0900

    네트워크 서브 시스템 구축

--- Demo/SagaGameWorld/Source/SagaFramework/Private/Saga/System/SagaGameManager.cpp
+++ Demo/SagaGameWorld/Source/SagaFramework/Private/Saga/System/SagaGameManager.cpp
@@ -1,9 +1,12 @@
 #include "Saga/System/SagaGameManager.h"
-#include "Saga/Network/SagaNetworkFunctionLibrary.h"
+#include "Saga/Network/SagaNetworkSubSystem.h"
 
 void
 USagaGameManager::Shutdown()
 {
 	Super::Shutdown();
-	USagaNetworkFunctionLibrary::SagaNetworkClose();
+
+	//auto net_system = GetSubsystem<USagaNetworkSubSystem>();
+
+	//net_system->Destroy();
 }
