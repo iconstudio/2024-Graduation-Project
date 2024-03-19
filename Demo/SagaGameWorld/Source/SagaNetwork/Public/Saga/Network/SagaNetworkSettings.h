b8e7aad51dcab9166deae04b6e827eddc824b4d7
iconer <yoyofa2@hotmail.com>
2024-03-20 02:17:44 +0900

    타입 수정

--- Demo/SagaGameWorld/Source/SagaNetwork/Public/Saga/Network/SagaNetworkSettings.h
+++ Demo/SagaGameWorld/Source/SagaNetwork/Public/Saga/Network/SagaNetworkSettings.h
@@ -6,7 +6,7 @@ namespace saga
 {
 	inline constexpr ESagaNetworkConnectionCategory ConnectionCategory = ESagaNetworkConnectionCategory::Host;
 	inline const FString RemoteAddress = TEXT("127.0.0.1");
-	inline constexpr int RemotePort = 40000U;
-	inline constexpr int LocalPort = 40001U;
+	inline constexpr int32 RemotePort = 40000U;
+	inline constexpr int32 LocalPort = 40001U;
 	inline constexpr bool IsOfflineMode = false;
 };
