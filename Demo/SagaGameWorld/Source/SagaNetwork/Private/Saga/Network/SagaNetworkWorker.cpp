d6068a6f1bbf39648d806f8d072d44693d74f7c4
iconer <yoyofa2@hotmail.com>
2024-03-21 05:52:04 +0900

    패킷 이벤트 처리는 네트워크 서브시스템에서 처리함

--- Demo/SagaGameWorld/Source/SagaNetwork/Private/Saga/Network/SagaNetworkWorker.cpp
+++ Demo/SagaGameWorld/Source/SagaNetwork/Private/Saga/Network/SagaNetworkWorker.cpp
@@ -4,8 +4,6 @@
 #include "Saga/Network/SagaNetworkSettings.h"
 #include "Saga/Network/SagaBasicPacket.h"
 #include "Saga/Network/SagaNetworkSubSystem.h"
-#include "Saga/Network/SagaRoomContract.h"
-#include "Saga/Network/SagaPacketProcessor.h"
 
 FSagaNetworkWorker::FSagaNetworkWorker(TObjectPtr<USagaNetworkSubSystem> instance)
 	: SubSystemInstance(MoveTempIfPossible(instance))
@@ -66,11 +64,15 @@ FSagaNetworkWorker::Run()
 				, temp_recv_bytes))
 			{
 				UE_LOG(LogSagaNetwork, Error, TEXT("Receiving has been failed!"));
+
+				SubSystemInstance->BroadcastOnDisconnected();
 				return 1;
 			}
 			else if (temp_recv_bytes <= 0)
 			{
 				UE_LOG(LogSagaNetwork, Error, TEXT("Received 0 byte!"));
+
+				SubSystemInstance->BroadcastOnDisconnected();
 				return 2;
 			}
 
@@ -93,7 +95,7 @@ FSagaNetworkWorker::Run()
 					auto ename = UEnum::GetValueAsString(basic_pk.myProtocol);
 					UE_LOG(LogSagaNetwork, Log, TEXT("Received a packet (%s)"), *ename);
 
-					RouteEvents(alt_buffer, basic_pk.myProtocol, basic_pk.mySize);
+					SubSystemInstance->RouteEvents(alt_buffer, basic_pk.myProtocol, basic_pk.mySize);
 
 					UE_LOG(LogSagaNetwork, Log, TEXT("Packet's size was %d"), basic_pk.mySize);
 
@@ -120,296 +122,4 @@ FSagaNetworkWorker::Exit()
 void
 FSagaNetworkWorker::Stop()
 {
-}
-
-void
-FSagaNetworkWorker::RouteEvents(const std::byte* packet_buffer, EPacketProtocol protocol, int16 packet_size)
-{
-	switch (protocol)
-	{
-	case EPacketProtocol::SC_SIGNIN_SUCCESS:
-	{
-		int32 my_id{};
-		saga::ReceiveSignInSucceedPacket(packet_buffer, my_id);
-
-		UE_LOG(LogSagaNetwork, Log, TEXT("Local client's id is %d"), my_id);
-
-		AsyncTask(ENamedThreads::GameThread
-			, [this, my_id]()
-			{
-				SubSystemInstance->SetLocalUserId(my_id);
-			}
-		);
-		SubSystemInstance->BroadcastOnConnected();
-	}
-	break;
-
-	case EPacketProtocol::SC_SIGNIN_FAILURE:
-	{
-		int32 error{};
-		saga::ReceiveSignInFailurePacket(packet_buffer, error);
-
-		UE_LOG(LogSagaNetwork, Log, TEXT("Local client can't get an id from server due to %d"), error);
-
-		AsyncTask(ENamedThreads::GameThread, [this]()
-			{
-				SubSystemInstance->BroadcastOnFailedToConnect(ESagaConnectionContract::SignInFailed);
-				SubSystemInstance->SetLocalUserId(-1);
-			}
-		);
-	}
-	break;
-
-	case EPacketProtocol::SC_ROOM_CREATED:
-	{
-		int32 room_id{};
-		saga::ReceiveRoomCreatedPacket(packet_buffer, room_id);
-
-		UE_LOG(LogSagaNetwork, Log, TEXT("A room %d is created"), room_id);
-
-		AsyncTask(ENamedThreads::GameThread, [this, room_id]()
-			{
-				SubSystemInstance->BroadcastOnRoomCreated(room_id);
-			}
-		);
-		SubSystemInstance->SetCurrentRoomId(room_id);
-	}
-	break;
-
-	case EPacketProtocol::SC_ROOM_CREATE_FAILED:
-	{
-		ERoomContract error{};
-		saga::ReceiveRoomCreationFailedPacket(packet_buffer, error);
-
-		const auto msg = std::to_wstring(error);
-		UE_LOG(LogSagaNetwork, Log, TEXT("Could not create a room due to %d"), msg.data());
-	}
-	break;
-
-	case EPacketProtocol::SC_ROOM_JOINED:
-	{
-		int32 newbie_id{};
-		int32 room_id{};
-		saga::ReceiveRoomJoinedPacket(packet_buffer, newbie_id, room_id);
-
-		if (newbie_id == SubSystemInstance->GetLocalUserId())
-		{
-			UE_LOG(LogSagaNetwork, Log, TEXT("Local client has joined to the room %d"), room_id);
-
-			AsyncTask(ENamedThreads::GameThread, [this, room_id, newbie_id]()
-				{
-					SubSystemInstance->SetCurrentRoomId(room_id);
-				}
-			);
-			SubSystemInstance->BroadcastOnJoinedRoom(newbie_id);
-		}
-		else
-		{
-			UE_LOG(LogSagaNetwork, Log, TEXT("Client %d has joined to the room %d"), newbie_id, room_id);
-
-			AsyncTask(ENamedThreads::GameThread, [this, newbie_id]()
-				{
-					SubSystemInstance->AddUser(FSagaVirtualUser{ newbie_id, TEXT("Member") });
-				}
-			);
-			SubSystemInstance->BroadcastOnJoinedRoom(newbie_id);
-		}
-	}
-	break;
-
-	case EPacketProtocol::SC_ROOM_JOIN_FAILED:
-	{
-		ERoomContract error{};
-		saga::ReceiveRoomJoinFailedPacket(packet_buffer, error);
-
-		const auto msg = std::to_wstring(error);
-		UE_LOG(LogSagaNetwork, Log, TEXT("Failed to join to a room due to %s"), msg.data());
-	}
-	break;
-
-	case EPacketProtocol::SC_ROOM_LEFT:
-	{
-		int32 left_client_id{};
-		saga::ReceiveRoomLeftPacket(packet_buffer, left_client_id);
-
-		if (left_client_id == SubSystemInstance->GetLocalUserId())
-		{
-			UE_LOG(LogSagaNetwork, Log, TEXT("Local client has been left from room"));
-
-			AsyncTask(ENamedThreads::GameThread, [this]()
-				{
-					SubSystemInstance->SetCurrentRoomId(-1);
-					SubSystemInstance->ClearUserList();
-				}
-			);
-			SubSystemInstance->BroadcastOnLeftRoomBySelf();
-		}
-		else
-		{
-			UE_LOG(LogSagaNetwork, Log, TEXT("Remote client %d has been left from room"), left_client_id);
-
-			AsyncTask(ENamedThreads::GameThread, [this, left_client_id]()
-				{
-					SubSystemInstance->RemoveUser(left_client_id);
-				}
-			);
-			SubSystemInstance->BroadcastOnLeftRoom(left_client_id);
-		}
-	}
-	break;
-
-	case EPacketProtocol::SC_RESPOND_VERSION:
-	{
-		wchar_t version_string[16]{};
-
-		saga::ReceiveRespondVersionPacket(packet_buffer, version_string, 16);
-
-		UE_LOG(LogSagaNetwork, Log, TEXT("Version: %s"), version_string);
-
-		SubSystemInstance->BroadcastOnRespondVersion(version_string);
-	}
-	break;
-
-	case EPacketProtocol::SC_RESPOND_ROOMS:
-	{
-		std::vector<saga::datagrams::SerializedRoom> rooms{};
-
-		saga::ReceiveRespondRoomsPacket(packet_buffer, rooms);
-
-		UE_LOG(LogSagaNetwork, Log, TEXT("Rooms: %d"), rooms.size());
-
-		AsyncTask(ENamedThreads::GameThread, [this, tr_rooms = std::move(rooms)]()
-			{
-				SubSystemInstance->ClearRoomList();
-				for (auto& room : tr_rooms)
-				{
-					SubSystemInstance->AddRoom(FSagaVirtualRoom
-						{
-							room.id, room.title, static_cast<int>(room.members)
-						});
-					UE_LOG(LogSagaNetwork, Log, TEXT("Room (%d): %s (%d/4)"), room.id, room.title, room.members);
-				}
-			}
-		);
-
-		SubSystemInstance->BroadcastOnUpdateRoomList(SubSystemInstance->everyRooms);
-	}
-	break;
-
-	case EPacketProtocol::SC_RESPOND_USERS:
-	{
-		std::vector<saga::datagrams::SerializedMember> users{};
-
-		saga::ReceiveRespondUsersPacket(packet_buffer, users);
-
-		UE_LOG(LogSagaNetwork, Log, TEXT("Members: %d"), users.size());
-
-		AsyncTask(ENamedThreads::GameThread, [this, tr_users = std::move(users)]()
-			{
-				SubSystemInstance->ClearUserList();
-				for (auto& user : tr_users)
-				{
-					SubSystemInstance->AddUser(FSagaVirtualUser
-						{
-							user.id, user.nickname
-						});
-
-					UE_LOG(LogSagaNetwork, Log, TEXT("User (%d): %s"), user.id, user.nickname);
-				}
-			}
-		);
-
-		SubSystemInstance->BroadcastOnUpdateMembers(SubSystemInstance->everyUsers);
-	}
-	break;
-
-	case EPacketProtocol::SC_FAILED_GAME_START:
-	{
-		saga::SC_FailedGameStartingPacket pk{};
-		auto offset = pk.Read(packet_buffer);
-
-		UE_LOG(LogSagaNetwork, Log, TEXT("Failed to start game due to %d"), pk.errCause);
-	}
-	break;
-
-	case EPacketProtocol::SC_GAME_GETTING_READY:
-	{
-		//SC_ReadyForGamePacket pk{};
-		//auto offset = pk.Read(packet_buffer);
-
-		UE_LOG(LogSagaNetwork, Log, TEXT("Now start loading game..."));
-	}
-	break;
-
-	case EPacketProtocol::SC_GAME_START:
-	{
-		//SC_GameStartPacket pk{};
-		//auto offset = pk.Read(packet_buffer);
-
-		UE_LOG(LogSagaNetwork, Log, TEXT("Now start game..."));
-	}
-	break;
-
-	case EPacketProtocol::SC_CREATE_PLAYER:
-	{
-		saga::SC_CreatePlayerPacket pk{};
-		auto offset = pk.Read(packet_buffer);
-
-		UE_LOG(LogSagaNetwork, Log, TEXT("A client %d is created"), pk.clientId);
-
-		AsyncTask(ENamedThreads::GameThread, [this, pk]()
-			{
-			}
-		);
-	}
-	break;
-
-	case EPacketProtocol::SC_REMOVE_PLAYER:
-	{
-		saga::SC_DestroyPlayerPacket pk{};
-		auto offset = pk.Read(packet_buffer);
-
-		UE_LOG(LogSagaNetwork, Log, TEXT("A client %d is destroyed(disconnected)"), pk.clientId);
-
-		AsyncTask(ENamedThreads::GameThread, [this, pk]()
-			{
-			}
-		);
-	}
-	break;
-
-	case EPacketProtocol::SC_MOVE_CHARACTER:
-	{
-		int32 client_id{};
-		float x, y, z;
-
-		saga::ReceivePositionPacket(packet_buffer, client_id, x, y, z);
-
-		UE_LOG(LogSagaNetwork, Log, TEXT("Client id %d: pos(%f,%f,%f)"), client_id, x, y, z);
-		UE_LOG(LogSagaNetwork, Log, TEXT("Client id pos"));
-
-		AsyncTask(ENamedThreads::GameThread, [this, client_id, x, y, z]()
-			{
-			}
-		);
-		SubSystemInstance->BroadcastOnUpdatePosition(client_id, x, y, z);
-	}
-	break;
-
-	case EPacketProtocol::SC_UPDATE_CHARACTER:
-	{
-	}
-	break;
-
-	case EPacketProtocol::SC_RPC:
-	{
-		saga::SC_RpcPacket pk{};
-
-		wchar_t buffer[sizeof(pk.rpcScript) / sizeof(wchar_t)]{};
-		std::copy(std::cbegin(pk.rpcScript), std::cend(pk.rpcScript), std::begin(buffer));
-
-		UE_LOG(LogSagaNetwork, Log, TEXT("RPC from client %d: %s(%lld))"), pk.clientId, pk.rpcScript, pk.rpcArgument);
-	}
-	break;
-	}
 }
