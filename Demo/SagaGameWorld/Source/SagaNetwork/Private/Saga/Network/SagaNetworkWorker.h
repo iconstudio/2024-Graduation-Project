d4f1b7adcbf4623a044cb048c5536a54b94a91c7
iconer <yoyofa2@hotmail.com>
2024-03-20 13:16:54 +0900

    네트워크 시스템 수정

--- Demo/SagaGameWorld/Source/SagaNetwork/Private/Saga/Network/SagaNetworkWorker.h
+++ Demo/SagaGameWorld/Source/SagaNetwork/Private/Saga/Network/SagaNetworkWorker.h
@@ -2,28 +2,4 @@
 #ifndef SAGAFRAMEWORK_NET_WORKER_H
 #define SAGAFRAMEWORK_NET_WORKER_H
 
-#include "CoreMinimal.h"
-#include "HAL/Runnable.h"
-#include "HAL/RunnableThread.h"
-
-class USagaNetworkSubSystem;
-
-class SAGANETWORK_API FSagaNetworkWorker final : public FRunnable
-{
-public:
-	FSagaNetworkWorker(TObjectPtr<USagaNetworkSubSystem> instance);
-	~FSagaNetworkWorker();
-
-	virtual bool Init() override;
-	virtual uint32 Run() override;
-	virtual void Exit() override;
-	virtual void Stop() override;
-
-private:
-	void RouteEvents(const std::byte* packet_buffer, EPacketProtocol protocol, int16 packet_size);
-
-	FRunnableThread* MyThread;
- 	TObjectPtr<USagaNetworkSubSystem> SubSystemInstance;
-};
-
 #endif // !SAGAFRAMEWORK_NET_WORKER_H
