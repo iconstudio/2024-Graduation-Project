d0a986101dff3ea981547cc2f46093317d1940d6
iconer <yoyofa2@hotmail.com>
2024-03-29 04:07:19 +0900

    문제되는 블루프링트 클래스 제거 작업 중

--- Demo/SagaGameWorld/Source/SagaNetwork/Public/Saga/Network/SagaNetworkSystem.h
+++ Demo/SagaGameWorld/Source/SagaNetwork/Public/Saga/Network/SagaNetworkSystem.h
@@ -4,11 +4,8 @@
 namespace saga
 {
 	class SAGANETWORK_API USagaNetwork final
-		: public TSharedFromThis<USagaNetwork>
 	{
 	public:
-		using Super = TSharedFromThis<USagaNetwork>;
-
 		[[nodiscard]] static bool Start(FStringView nickname) noexcept { return true; }
 	};
 }
