2b5873f7949b8a736ccffcbf4dbd5e61bcd35cd0
iconstudio <yoyofa2@hotmail.com>
2024-03-31 19:37:04 +0900

    잘못된 팀 정보를 보내는 버그 수정

--- Server/Demo/FrameworkEvents1.cpp
+++ Server/Demo/FrameworkEvents1.cpp
@@ -200,6 +200,7 @@ demo::Framework::OnNotifyTeam(iconer::app::User& user)
 {
 	const auto room_id = user.myRoomId.Load();
 	const auto& user_id = user.GetID();
+	const auto team_id = user.myTeamId.Load();
 
 	if (room_id != -1)
 	{
@@ -222,12 +223,12 @@ demo::Framework::OnNotifyTeam(iconer::app::User& user)
 			else
 			{
 				room->ForEach(
-					[](iconer::app::User& user)
+					[user_id, team_id](iconer::app::User& user)
 					{
-						auto [io, ctx] = user.SendChangeTeamPacket(user.GetID(), user.myTeamId == iconer::app::Team::Red);
+						auto [io, ctx] = user.SendChangeTeamPacket(user_id, team_id == iconer::app::Team::Red);
 						if (not io)
 						{
-							delete ctx;
+							ctx.Complete();
 						};
 					}
 				);
